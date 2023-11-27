89f2d222df702d183b7603966b13c93378caa863
raphavidall <rapha.vidall@gmail.com>
2023-11-27 19:54:55 -0300

    feature: func. middleware de auttenticacao jwt.

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -1,6 +1,40 @@
-const middleware = (req, res, next) => {
-    console.log("MIDDLEWARE");
-    next();
+const jwt = require('jsonwebtoken');
+const knex = require('../config/connection');
+
+const autenticacao = async (req, res, next) => {
+
+    const { authorization } = req.headers;
+
+    if(!autenticacao){
+        return res.status(400).json({message: `Usuário não autorizado.`})
+    }
+
+    const token = authorization.split(" ")[1];
+
+    try {
+
+        const { id } = await jwt.verify(token, process.env.JWTPASS);
+
+        const authUser = await knex.select('*').from('usuarios').where({id});
+
+        console.log(authUser);
+
+        if(!authUser){
+            return res.status(404).json({message: `Usuário não autorizado.`});
+        }
+
+        const { senha:_, ...usuario } = authUser;
+
+        req.usuario = usuario;
+
+        next();
+
+    } catch (error) {
+        
+        return res.status(500).json({message: `Erro interno do servidor (middleware autorização).`})
+
+    }
+
 }
 
-module.exports = middleware;
+module.exports = autenticacao;
